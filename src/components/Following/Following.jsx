efc24247ad59dd08559cdef9ea4739275d1f39a1
Jegathish-16 <jegathish162004@gmail.com>
2024-06-23 17:33:12 +0530

    Add Following component

---
+++ src/components/Following/Following.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+// import { HiChevronDown } from 'react-icons/hi';
+import './Following.css';
+import links from './links';
+
+const followingLinks = [
+    'Following',
+    'Popular',
+    'New'
+];
+
+function Following({ onSelectCategory, selectedCategory }) {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleCategoryClick = (category) => {
+        onSelectCategory(category);
+        setShowDropdown(false);
+    };
+    
+
+    return (
+        <div className='categories-container'>
+            <div className='following-container'>
+            <select onChange={(e)=>handleCategoryClick(e.target.value)}>
+                    {followingLinks.map((link,index) => (
+                        <option key={index} value={link.toLowerCase()}>{link}</option>
+                    ))}
+            </select>
+            </div>
+            <div className='category-links'>
+                {links.map((link) => (
+                    <button
+                        key={link.name}
+                        className={`category-button ${selectedCategory === link.name ? 'selected' : ''}`}
+                        onClick={() => handleCategoryClick(link.name)}
+                    >
+                        {link.name}
+                    </button>
+                ))}
+            </div>
+            <div className='filter'>
+                <div>Filters</div>
+            </div>
+        </div>
+    );
+}
+
+export default Following;
---
+++ src/components/Following/links.jsx
@@ -0,0 +1,51 @@
+const links = [
+    {
+        name: 'Discover',
+        route: '/discover'
+    },
+
+    {
+        name: 'Animation',
+        route: '/animation'
+    },
+
+    {
+        name: 'Branding',
+        route: '/branding'
+    },
+
+    {
+        name: 'Illustration',
+        route: '/illustration'
+    },
+
+    {
+        name: 'Mobile',
+        route: '/mobile'
+    },
+
+    
+    {
+        name: 'Print',
+        route: '/print'
+    },
+
+    
+    {
+        name: 'Product Design',
+        route: '/product-design'
+    },
+
+    
+    {
+        name: 'Typography',
+        route: '/typography'
+    },
+
+    {
+        name: 'Web Design',
+        route: '/web-design'
+    },
+]
+
+export default links
